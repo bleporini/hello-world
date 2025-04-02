78676a6bf4fdb7f81f045dba4a3b474b3083fd54
Brice LEPORINI <bleporini@gmail.com>
2025-04-02 16:28:06 +0400

    initial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new HelloHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+        System.out.println("Server started on port 8080");
+    }
+
+    static class HelloHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            if ("GET".equals(exchange.getRequestMethod())) {
+                String response = "Hello, world!\n  ";
+                exchange.sendResponseHeaders(200, response.getBytes().length);
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            } else {
+                exchange.sendResponseHeaders(405, -1); // 405 Method Not Allowed
+            }
+
+        }
+
+    }
+}
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void smokeTest() {
+        assertTrue(true);
+//        throw new RuntimeException("you shall not pass");
+    }
+}
